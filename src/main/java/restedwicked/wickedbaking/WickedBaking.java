d87d3c7a148cc8ff8eab7f488551d6d8957ceaf6
J. David Perez <69410942+RestedWickedDev@users.noreply.github.com>
2022-11-09 04:00:25 -0800

    Start

---
+++ src/main/java/restedwicked/wickedbaking/WickedBaking.java
@@ -0,0 +1,17 @@
+package restedwicked.wickedbaking;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WickedBaking extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
